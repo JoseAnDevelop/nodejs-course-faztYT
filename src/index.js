94cbc52b72325c8aa16f792980602fe55b9ef339
José AndRam <69285944+JoseAnDevelop@users.noreply.github.com>
2023-12-16 12:45:02 -0600

    Cambios en configuracion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ app.get('/about', (req, res) => {
     res.send('<h1>About us</h1>')
 })
 
-app.listen(3000)
-console.log('Listening on port 3000')
+/* app.listen(3000)
+console.log('Listening on port 3000') */
 
+// Heroku te asigna un puerto, en caso de que no, por default el 3000
+app.listen(process.env.PORT || 3000)
+console.log('Listening on port ', process.env.PORT || 3000)
+
+/* En archivo package.json, agregamos el script start con node y no con nodemon, ya que en el host se usa node, y nodemon es para ambiente dev */
